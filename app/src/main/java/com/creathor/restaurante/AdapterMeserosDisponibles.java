5e75aa44a725d50040c12ce5ec21bf5eb4104485
kaaytsaak <eggman2000>
2022-12-04 02:17:51 -0600

    restaurante4

--- app/src/main/java/com/creathor/restaurante/AdapterMeserosDisponibles.java
+++ app/src/main/java/com/creathor/restaurante/AdapterMeserosDisponibles.java
@@ -1,11 +1,14 @@
 package com.creathor.restaurante;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -14,13 +17,14 @@ import java.util.ArrayList;
 
 public class AdapterMeserosDisponibles extends RecyclerView.Adapter<AdapterMeserosDisponibles.ViewHolderRecycler>{
 private ArrayList<ListaMeserosDisponibles> meserosDisponiblesRecycler;
-        ViewHolderRecycler viewholderListaMeserosDisponibles;
+        ViewHolderRecycler viewholderListaMeserosDisponibles,anterior;
 private  RecyclerView recyclerView;
 private Context context;
 private String id_meseros, nombre_mesero;
 private TextView aceptar_pedido;
 private AdapterListaPedidos activity;
-
+private SharedPreferences datosMeseros;
+private SharedPreferences.Editor editorMeseros;
 
 public AdapterMeserosDisponibles(ArrayList<ListaMeserosDisponibles> meseroDisponiblerecycler )
         {
@@ -31,23 +35,52 @@ public ViewHolderRecycler onCreateViewHolder(ViewGroup parent, int viewType) {
         View vista = LayoutInflater.from(parent.getContext()).inflate(R.layout.item3,parent,false);
         context=parent.getContext();
         vista.setFocusable(true);
+         datosMeseros= context.getSharedPreferences("meserosDisponibles",context.MODE_PRIVATE);
+        editorMeseros=datosMeseros.edit();
         return new ViewHolderRecycler(vista);
 
         }
 
 @Override
-public void onBindViewHolder(@NonNull AdapterMeserosDisponibles.ViewHolderRecycler holder, int position) {
+public void onBindViewHolder(@NonNull ViewHolderRecycler holder, int position) {
         viewholderListaMeserosDisponibles =holder;
         id_meseros = meserosDisponiblesRecycler.get(position).getId_mesero();
         nombre_mesero = meserosDisponiblesRecycler.get(position).getNombre_mesero();
-
-
-
-
-
         holder.id_meser.setText(id_meseros);
         holder.name_meser.setText(nombre_mesero);
 
+        final String nombreMesero  =  holder.name_meser.getText().toString();
+        holder.caja_marco_mesero.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                Toast.makeText(context,nombreMesero,Toast.LENGTH_SHORT).show();
+                editorMeseros.putString("meserosDisponibles",nombreMesero);
+                editorMeseros.apply();
+                Log.e("cambio",datosMeseros.getString("meserosDisponibles","no"));
+                Log.e("index", String.valueOf(holder.getAdapterPosition()));
+
+                int posicion_tmp= holder.getAdapterPosition();
+
+                if (anterior!=null)
+                {
+                    anterior.caja_marco_mesero.setBackgroundResource(R.color.white);
+                    anterior.name_meser.setTextColor(context.getResources().getColor(R.color.black));
+                }
+
+                Log.e("holdr", String.valueOf(holder));
+
+                holder.caja_marco_mesero.setBackgroundResource(R.color.black);
+                holder.name_meser.setTextColor(context.getResources().getColor(R.color.white));
+
+
+                /*((Estacion)context).definirAlcance(nombreMesero);*/
+
+                anterior=holder;
+
+            }
+        });
         //holder.date_end.setText(fecha_final);
 
 
@@ -66,13 +99,13 @@ public int getItemCount(){
         }
 public class ViewHolderRecycler extends RecyclerView.ViewHolder {
     TextView id_meser,name_meser;
-
+    LinearLayout caja_marco_mesero;
 
     public ViewHolderRecycler(View itemView) {
         super(itemView);
         id_meser =(TextView)itemView.findViewById(R.id.id_mesero);
         name_meser =(TextView)itemView.findViewById(R.id.nombre_mesero);
-
+        caja_marco_mesero=itemView.findViewById(R.id.caja_marco_mesero);
     }
 }
 
